e0d847219fe5c22638ab20b160919f0c400b996d
sffranke <sffranke@github.com>
2022-04-03 12:11:02 +0200

    start impl. of inverse kinematics

--- pyro.py
+++ pyro.py
@@ -3,7 +3,8 @@ import sys
 import Adafruit_PCA9685
 import time
 from Classes.TheThreads import MyThread
-from math import sin, cos, pi, sqrt
+#from math import sin, cos, pi, sqrt
+from numpy import *
 from pyPS4Controller.controller import Controller
 
 import configparser
@@ -12,13 +13,13 @@ import os
 configfile_name = "/home/ubuntu/pyro/config.ini"
 
 class Servo:
-    def __init__(self, name, servopin, minangle, maxangle, center_pwm, center_degrees, transition, transitionspeed, servorotation, area):
+    def __init__(self, name, servopin, minangle, maxangle, center_pwm, transition, transitionspeed, servorotation, area):
         self.__name = name
         self.__servopin = servopin
         self.__minangle = minangle
         self.__maxangle = maxangle
         self.__center_pwm = center_pwm
-        self.__center_degrees = center_degrees
+        self.__center_degrees = 0
         self.__transition = transition
         self.__transitionspeed = transitionspeed
         self.__servorotation = servorotation
@@ -38,12 +39,12 @@ class Servo:
         
     def GetServopin(self):
         return self.__servopin
+    
+    def GetCenterDegrees(self):
+        return self.__center_degrees
         
     def GetArea(self):
         return self.__area
-        
-    def GetCenterDegrees(self):
-        return self.__center_degrees
         
     def GetMaxAngle(self):
         return self.__minangle
@@ -152,55 +153,15 @@ class Servo:
         
     def deg2pwm(self, angle, rotation):
         if rotation == 1:
-            r= angle*(self.__maxangle-self.__minangle)/self.GetMaxAngle() + self.__center_pwm
+            pwm= angle*(self.__maxangle-self.__minangle)/self.GetMaxAngle() + self.__center_pwm
         else:
-            r=-angle*(self.__maxangle-self.__minangle)/self.GetMaxAngle() + self.__center_pwm
+            pwm=-angle*(self.__maxangle-self.__minangle)/self.GetMaxAngle() + self.__center_pwm
         
-        return(int(r))
+        return(int(pwm))
         
     def angle(self):
         return self.__angle
 
-
-#
-# Check if there is already a configurtion file
-if not os.path.isfile(configfile_name):
-    # Cre ate the configuration file as it doesn't exist yet
-    cfgfile = open(configfile_name, 'w')
-    # Add content to the file
-    Config = configparser.ConfigParser()
-    Config.add_section("LF")
-    Config.set("LF", "Coxa-center", "306")
-    Config.set("LF", "Coxa-center-dif", "0")
-    Config.set("LF", "Femur-center", "306")
-    Config.set("LF", "Femur-center-dif", "0")
-    Config.set("LF", "Tibia-center", "306")
-    Config.set("LF", "Tibia-center-dif", "0")
-    Config.add_section("RF")
-    Config.set("RF", "Coxa-center", "306")
-    Config.set("RF", "Coxa-center-dif", "0")
-    Config.set("RF", "Femur-center", "306")
-    Config.set("RF", "Femur-center-dif", "0")
-    Config.set("RF", "Tibia-center", "306")
-    Config.set("RF", "Tibia-center-dif", "0")
-    Config.add_section("RR")
-    Config.set("RR", "Coxa-center", "306")
-    Config.set("RR", "Coxa-center-dif", "0")
-    Config.set("RR", "Femur-center", "306")
-    Config.set("RR", "Femur-center-dif", "0")
-    Config.set("RR", "Tibia-center", "306")
-    Config.set("RR", "Tibia-center-dif", "0")
-    Config.add_section("RL")
-    Config.set("RL", "Coxa-center", "306")
-    Config.set("RL", "Coxa-center-dif", "0")
-    Config.set("RL", "Femur-center", "306")
-    Config.set("RL", "Femur-center-dif", "0")
-    Config.set("RL", "Tibia-center", "306")
-    Config.set("RL", "Tibia-center-dif", "0")
-           
-    Config.write(cfgfile)
-    cfgfile.close()
-        
 # Load the configuration file
 with open(configfile_name) as f:
     sample_config = f.read()
@@ -220,21 +181,36 @@ with open(configfile_name) as f:
     '''
 
     Coxa_LF_center_pwm= int(config.get("LF", "Coxa-center")) +int(config.get("LF", "Coxa-center-dif"))
+    Coxa_LF_length = int(config.get("LF", "Coxa-length"))
+    Coxa_LF_maxangle = int(config.get("LF", "Coxa-maxangle"))
+    Coxa_LF_minangle = int(config.get("LF", "Coxa-minangle"))
+    
     Femur_LF_center_pwm=int(config.get("LF", "Femur-center"))+int(config.get("LF", "Femur-center-dif"))
-    Tiba_LF_center_pwm= int(config.get("LF", "Tibia-center"))+int(config.get("LF", "Tibia-center-dif"))
+    Femur_LF_length = int(config.get("LF", "Femur-length"))
+    Femur_LF_maxangle = int(config.get("LF", "Femur-maxangle"))
+    Femur_LF_minangle = int(config.get("LF", "Femur-minangle"))
+    
+    Tibia_LF_center_pwm= int(config.get("LF", "Tibia-center"))+int(config.get("LF", "Tibia-center-dif"))
+    Tibia_LF_length = int(config.get("LF", "Tibia-length"))
+    Tibia_LF_maxangle = int(config.get("LF", "Tibia-maxangle"))
+    Tibia_LF_minangle = int(config.get("LF", "Tibia-minangle"))
+    
     Coxa_RF_center_pwm= int(config.get("RF", "Coxa-center")) +int(config.get("RF", "Coxa-center-dif"))
-    
-#
+    Coxa_RF_length = int(config.get("RF", "Coxa-length"))
+    Coxa_RF_maxangle = int(config.get("RF", "Coxa-maxangle"))
+    Coxa_RF_minangle = int(config.get("RF", "Coxa-minangle"))
+ 
+    Conf_speed = float(config.get("GENERAL", "speed"))
+#maxangle= 80
+#minangle=-80
+speed = Conf_speed
 
-maxangle= 80
-minangle=-80
-speed=1
 servos = [
-    Servo(name="Coxa_LF",  servopin=0, minangle=minangle, maxangle=maxangle, center_pwm=Coxa_LF_center_pwm, center_degrees=0, transition=0,  transitionspeed=speed, servorotation=  1, area=45),
-    Servo(name="Femur_LF", servopin=1, minangle=minangle, maxangle=maxangle, center_pwm=Femur_LF_center_pwm, center_degrees=0, transition=0,  transitionspeed=speed, servorotation=  1, area=45),
-    Servo(name="Tibia_LF", servopin=2, minangle=minangle, maxangle=maxangle, center_pwm=Tiba_LF_center_pwm, center_degrees=0, transition=0,  transitionspeed=speed, servorotation=  1, area=45),
+    Servo(name="Coxa_LF",  servopin=0, minangle=Coxa_LF_minangle, maxangle=Coxa_LF_maxangle, center_pwm=Coxa_LF_center_pwm, transition=0,  transitionspeed=speed, servorotation=  1, area=45),
+    Servo(name="Femur_LF", servopin=1, minangle=Femur_LF_minangle, maxangle=Femur_LF_maxangle, center_pwm=Femur_LF_center_pwm, transition=0,  transitionspeed=speed, servorotation=  1, area=45),
+    Servo(name="Tibia_LF", servopin=2, minangle=Tibia_LF_minangle, maxangle=Tibia_LF_maxangle, center_pwm=Tibia_LF_center_pwm, transition=0,  transitionspeed=speed, servorotation=  1, area=45),
     
-    Servo(name="Coxa_RF",  servopin=3, minangle=minangle, maxangle=maxangle, center_pwm=Coxa_RF_center_pwm, center_degrees=0, transition=0,  transitionspeed=speed, servorotation= -1, area=45),
+    Servo(name="Coxa_RF",  servopin=3, minangle=Coxa_RF_minangle, maxangle=Coxa_LF_maxangle, center_pwm=Coxa_RF_center_pwm, transition=0,  transitionspeed=speed, servorotation= -1, area=45),
     ]
 
 
@@ -290,7 +266,71 @@ def test():
     pyrothred_FEMUR_FRONT_LEFT.start()
     pyrothred_TIBIA_FRONT_LEFT = MyThread(target=servo.move, args=(Tibia_LF.GetServopin(), -45 , Tibia_LF, transition ))
     pyrothred_TIBIA_FRONT_LEFT.start()
+
+def get_IK_angles(drop, slew):
+
+        l_femur = Femur_LF_length*10 # mm
+        l_tibia = Tibia_LF_length*10 # mm
+
+        hypotenuse = sqrt(drop**2+slew**2)
+        phi_1 = arccos((hypotenuse**2 + l_femur**2-l_tibia**2)/(2*hypotenuse*l_femur))
+        phi_2 = arctan(slew/drop)
+        hip = rad2deg(phi_2-phi_1)
+        knee = rad2deg(arccos((l_femur**2+l_tibia**2-hypotenuse**2)/(2*l_femur*l_tibia)))
+        
+        return [0, int(hip-90), int(knee-90)]
+
+"""
+INVERSE KINEMATICS by https://github.com/miguelasd688/Quadruped-dog-like-robot/blob/master/src/quadpod.py
+"""
+def checkdomain(D):
+    if D > 1 or D < -1:
+        print("____OUT OF DOMAIN____")
+        if D > 1: 
+            D = 0.99
+            return D
+        elif D < -1:
+            D = -0.99
+            return D
+    else:
+        return D
+        
+
+def solveIK_R(coord , coxa , femur , tibia): 
+"""where:
+   coord = np.array([x4,y4,z4])
+   coxa = L1 
+   femur = L2 
+   tibia = L3
+"""
+
+    D = (coord[1]**2+(-coord[2])**2-coxa**2+(-coord[0])**2-femur**2-tibia**2)/(2*tibia*femur)  
+    D = checkdomain(D) #This is just to deal with NaN solutions
+    gamma = numpy.arctan2(-numpy.sqrt(1-D**2),D)
+    tetta = -numpy.arctan2(coord[2],coord[1])-numpy.arctan2(numpy.sqrt(coord[1]**2+(-coord[2])**2-coxa**2),-coxa)
+    alpha = numpy.arctan2(-coord[0],numpy.sqrt(coord[1]**2+(-coord[2])**2-coxa**2))-numpy.arctan2(tibia*numpy.sin(gamma),femur+tibia*numpy.cos(gamma))
+    angles = numpy.array([-tetta, alpha, gamma])
+    return angles
+
+def ik(height, xpos):
+
+        #arr = get_IK_angles(height,xpos)
+        arr = solveIK_L([x4,y4,z4],5, 5, 1)
+        print ("arr: ", arr)
+        #return
     
+        COXA_FRONT_LEFT_pos_angle  = arr[0]
+        FEMUR_FRONT_LEFT_pos_angle = arr[1]
+        TIBIA_FRONT_LEFT_pos_angle = arr[2]
+        
+        transition=0
+        pyrothred_COXA_FRONT_LEFT = MyThread(target=servo.move, args=(Coxa_LF.GetServopin(), COXA_FRONT_LEFT_pos_angle, Coxa_LF, transition ))
+        pyrothred_COXA_FRONT_LEFT.start()   
+        pyrothred_FEMUR_FRONT_LEFT = MyThread(target=servo.move, args=(Femur_LF.GetServopin(), FEMUR_FRONT_LEFT_pos_angle, Femur_LF, transition ))
+        pyrothred_FEMUR_FRONT_LEFT.start()
+        pyrothred_TIBIA_FRONT_LEFT = MyThread(target=servo.move, args=(Tibia_LF.GetServopin(), TIBIA_FRONT_LEFT_pos_angle, Tibia_LF, transition ))
+        pyrothred_TIBIA_FRONT_LEFT.start()
+        
     
 def release():
     for i in range(len(servos)):
@@ -307,14 +347,14 @@ def calibrate():
         print("Status: " ,servos[i].GetStatus())
         pyrothred_COXA_FRONT_LEFT = MyThread(target=servo.move, args=(Coxa_LF.GetServopin(), Coxa_LF.GetCenterDegrees() , Coxa_LF, transition ))
         pyrothred_COXA_FRONT_LEFT.start()   
-        pyrothred_FEMUR_FRONT_LEFT = MyThread(target=servo.move, args=(Femur_LF.GetServopin(), 0 , Femur_LF, transition ))
+        pyrothred_FEMUR_FRONT_LEFT = MyThread(target=servo.move, args=(Femur_LF.GetServopin(), Femur_LF.GetCenterDegrees() , Femur_LF, transition ))
         pyrothred_FEMUR_FRONT_LEFT.start()
-        pyrothred_TIBIA_FRONT_LEFT = MyThread(target=servo.move, args=(Tibia_LF.GetServopin(), 0 , Tibia_LF, transition ))
+        pyrothred_TIBIA_FRONT_LEFT = MyThread(target=servo.move, args=(Tibia_LF.GetServopin(), Tibia_LF.GetCenterDegrees() , Tibia_LF, transition ))
         pyrothred_TIBIA_FRONT_LEFT.start()
     
 
 #arg=sys.argv[1]
-arg = "c"
+arg = "i"
 '''
 if len(sys.argv) >2:
     limb = sys.argv[2]
@@ -322,6 +362,8 @@ if len(sys.argv) >2:
 
 if arg=="i":
     initialize()
+    time.sleep(1)
+    ik(80,0)  # mm
 
 if arg=="r":
     release()
