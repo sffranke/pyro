af622313a2b03bd8c6913507479cccb5243ad6e9
raincoat <53009955+sffranke@users.noreply.github.com>
2022-05-23 21:26:29 +0200

    Update plot_body.py

--- plot_body.py
+++ plot_body.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import random
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
@@ -8,7 +8,36 @@ from math import pi
 from spot_micro_kinematics_python.spot_micro_stick_figure import SpotMicroStickFigure
 from spot_micro_kinematics_python.utilities import spot_micro_kinematics as smk
 
+from pyPS4Controller.controller import Controller
+
 import time
+
+from threading import Timer
+
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 plt.ion() 
 
@@ -26,19 +55,12 @@ fig = plt.figure()
 # Instantiate spot micro stick figure obeject
 sm = SpotMicroStickFigure(x=0,y=0.18,z=0, theta=00*d2r)
 
-# # Try setting each leg to a desired y position
-# x4 = -.055
-# y4 = -.18
-# z4 = 0
-
 # Define absolute position for the legs
 l = sm.body_length
 w = sm.body_width
 l1 = sm.hip_length
 l2 = sm.upper_leg_length
 l3 = sm.lower_leg_length
-
-#def plotme():
 
 rrx = -l/2
 rfx =  l/2
@@ -55,10 +77,21 @@ rfz = w/2 + l1
 lfz = -w/2 - l1
 lrz = -w/2 - l1
 
+theta= 10*d2r
+psi= 0*d2r
+phi= 0*d2r
 
+arr= np.array([ [rrx,   rry,  rrz],
+                [rfx ,  rfy,  rfz],
+                [lfx ,  lfy,  lfz],
+                [lrx ,  lry,  lrz] ])
 
-
-def plotme(theta, psi, phi, arr):
+def plotme(msg):
+    global theta, psi, phi
+    print ("==========================")
+    print ("in plotme:", msg)
+    print ("theta: ", theta,"psi: ", psi, "phi: ",phi)
+    print ("==========================")
     ax = p3.Axes3D(fig)
     ax.cla()
 
@@ -115,113 +148,222 @@ def plotme(theta, psi, phi, arr):
     plt.pause(0.0001) 
 
 
-theta = 0*d2r  #pitch
-psi   = 0*d2r   #yaw
-phi   = 0*d2r  #roll
-#plotme(theta, psi, phi, arr) 
-#time.sleep(0.5)
-n = 0
-while n < 1:
-    print("n: ",n)
-    n += 1
+def reset():
+    global arr
+    global rrx, rry, rrz
+    global rfx, rfy, rfz
+    global lfx, lfy, lfz
+    global lrx, lry, lrz
+    
+    rrx = -l/2
+    rfx =  l/2
+    lfx =  l/2
+    lrx = -l/2
 
-    # Try leg to a desired position
-    x4 = 0
-    y4 = -0.18
-    z4 = 00.05
-        
-    for i in range(0, 40, 8):
-        theta = 10*d2r  #pitch
-        psi   = 0*d2r   #yaw
-        phi   = 0*d2r   #roll
-        
-        arr= np.array([ [rrx+i/1000,   rry+0.03,  rrz],
-                        [rfx ,  rfy,  rfz],
-                        [lfx ,  lfy,  lfz],
-                        [lrx ,  lry,  lrz] ])
-        
-        (q1,q2,q3) = smk.ikine(rrx+i/1000,rry+0.03,rrz,l1,l2,l3,legs12 = True)
+    rry = 0
+    rfy = 0
+    lfy = 0
+    lry = 0
 
-        print('Leg angles')
-        print('q1: %2.1f deg, q2: %2.1f deg, q3: %2.1f deg'%(q1*r2d,q2*r2d,q3*r2d))
-        
-        plotme(theta, psi, phi, arr)
-        #time.sleep(0.2)
-        print(i)
+    rrz = w/2 + l1
+    rfz = w/2 + l1
+    lfz = -w/2 - l1
+    lrz = -w/2 - l1
+
+    theta= 10*d2r
+    psi= 0*d2r
+    phi= 0*d2r
+
+    arr= np.array([ [rrx,   rry,  rrz],
+		     [rfx ,  rfy,  rfz],
+		     [lfx ,  lfy,  lfz],
+		     [lrx ,  lry,  lrz] ])
 
 
-    for i in range(40, -40, -8):
-        theta = 10*d2r  #pitch
-        psi   = 0*d2r   #yaw
-        phi   = 0*d2r   #roll
+def stand():
+    # Stand
+                 
+    # Get leg angles
+    leg_angs = sm.get_leg_angles()
+    print('Leg angles: ', leg_angs[0][0]*r2d, leg_angs[0][1]*r2d, leg_angs[0][2]*r2d)
+    print('Leg angles: ', leg_angs[1][0]*r2d, leg_angs[1][1]*r2d, leg_angs[1][2]*r2d)
+    print('Leg angles: ', leg_angs[2][0]*r2d, leg_angs[2][1]*r2d, leg_angs[2][2]*r2d)
+    print('Leg angles: ', leg_angs[3][0]*r2d, leg_angs[3][1]*r2d, leg_angs[3][2]*r2d)
+    #print('q1: %2.1f deg, q2: %2.1f deg, q3: %2.1f deg'%(q1*r2d,q2*r2d,q3*r2d))
         
-        arr= np.array([ [rrx+i/1000,   rry,  rrz],
-                        [rfx ,  rfy,  rfz],
-                        [lfx ,  lfy,  lfz],
-                        [lrx ,  lry,  lrz] ])
-        
-        plotme(theta, psi, phi, arr)
-        #time.sleep(0.2)
-        print(i)
+                
+rt = RepeatedTimer(3, plotme, "hey")
 
-    for i in range(-40, 0, 8):
-        theta = 10*d2r  #pitch
-        psi   = 0*d2r   #yaw
-        phi   = 0*d2r   #roll
-        
-        arr= np.array([ [rrx+i/1000,   rry+0.03,  rrz],
-                        [rfx ,  rfy,  rfz],
-                        [lfx ,  lfy,  lfz],
-                        [lrx ,  lry,  lrz] ])
-        
-        plotme(theta, psi, phi, arr)
-        #time.sleep(0.2)
-        print(i)
+def left_arrow_press(self):
+    global arr
+    global rrx, rry, rrz
+    global rfx, rfy, rfz
+    global lfx, lfy, lfz
+    global lrx, lry, lrz
+    
+    zmax = 0.02 #meter
+    delta = 0.004
+    rry = rry-delta
+    rfy = rfy-delta
+    lfy = lfy+delta
+    lry = lry+delta
 
-#plt.show()
-time.sleep(2)
+    arr= np.array([ [rrx,   rry,  rrz],
+                    [rfx ,  rfy,  rfz],
+                    [lfx ,  lfy,  lfz],
+                    [lrx ,  lry,  lrz] ])
 
-theta = 10*d2r  #pitch
-psi   = 0*d2r   #yaw
-phi   = 0*d2r   #roll
+def right_arrow_press(self):
+    global arr
+    global rrx, rry, rrz
+    global rfx, rfy, rfz
+    global lfx, lfy, lfz
+    global lrx, lry, lrz
+    
+    zmax = 0.02 #meter
+    delta = 0.004
+    rry = rry+delta
+    rfy = rfy+delta
+    lfy = lfy-delta
+    lry = lry-delta
+
+    arr= np.array([ [rrx,   rry,  rrz],
+                    [rfx ,  rfy,  rfz],
+                    [lfx ,  lfy,  lfz],
+                    [lrx ,  lry,  lrz] ])
+
+def up_arrow_press(self):
+    global arr
+    global rrx, rry, rrz
+    global rfx, rfy, rfz
+    global lfx, lfy, lfz
+    global lrx, lry, lrz
+    
+    zmax = 0.02 #meter
+    delta = 0.004
+    rry = rry-delta
+    rfy = rfy-delta
+    lfy = lfy-delta
+    lry = lry-delta
+
+    arr= np.array([ [rrx,   rry,  rrz],
+                    [rfx ,  rfy,  rfz],
+                    [lfx ,  lfy,  lfz],
+                    [lrx ,  lry,  lrz] ])
+
+def down_arrow_press(self):
+    global arr
+    global rrx, rry, rrz
+    global rfx, rfy, rfz
+    global lfx, lfy, lfz
+    global lrx, lry, lrz
+    
+    zmax = 0.02 #meter
+    delta = -0.004
+    rry = rry-delta
+    rfy = rfy-delta
+    lfy = lfy-delta
+    lry = lry-delta
+
+    arr= np.array([ [rrx,   rry,  rrz],
+                    [rfx ,  rfy,  rfz],
+                    [lfx ,  lfy,  lfz],
+                    [lrx ,  lry,  lrz] ])
+                    
+def leftjoyleft(self, value):
+    global phi
+    phi_max=20
+    phi_min=0
+    # 0 - 32767
+    para = int( 100*value/32767)  # in %
+    phi=(para/100)*phi_max*d2r
+
+def leftjoyright(self, value):
+    global phi
+    phi_max=20
+    phi_min=0
+    # 0 - 32767
+    para = int( 100*value/32767)  # in %
+    phi=(para/100)*phi_max*d2r
+
+def leftjoyup(self, value):
+    global theta
+    theta_max=20
+    theta_min=0
+    # 0 - 32767
+    para = int( 100*value/32767)  # in %
+    theta=(para/100)*theta_max*d2r
+
+def leftjoydown(self, value):
+    global theta
+    theta_max=20
+    theta_min=0
+    # 0 - 32767
+    para = int( 100*value/32767)  # in %
+    theta=(para/100)*theta_max*d2r
+
+def rightjoyleft(self, value):
+    global psi
+    psi_max=20
+    psi_min=0
+    # 0 - 32767
+    para = int( 100*value/32767)  # in %
+    psi=(para/100)*psi_max*d2r
+
+def rightjoyright(self, value):
+    global psi
+    psi_max=20
+    psi_min=0
+    # 0 - 32767
+    para = int( 100*value/32767)  # in %
+    psi=(para/100)*psi_max*d2r
+
+### run PS4Controller   
+class MyController(Controller):
+    def __init__(self, **kwargs):
+        Controller.__init__(self, **kwargs)
+    
+    def on_share_press(self):
+    	reset()
+    
+    def on_left_arrow_press(self):
+        left_arrow_press("left_arrow_press")
         
-arr= np.array([ [rrx,   rry,  rrz],
-                        [rfx ,  rfy,  rfz],
-                        [lfx ,  lfy,  lfz],
-                        [lrx ,  lry,  lrz] ])
+    def on_right_arrow_press(self):
+        right_arrow_press("right_arrow_press")
+    
+    def on_up_arrow_press(self):
+        up_arrow_press("up_arrow_press")
         
-(q1,q2,q3) = smk.ikine(rrx+i/1000,rry+0.03,rrz,l1,l2,l3,legs12 = True)
-
-print('Leg angles: ')
-print('q1: %2.1f deg, q2: %2.1f deg, q3: %2.1f deg'%(q1*r2d,q2*r2d,q3*r2d))
+    def on_down_arrow_press(self):
+        down_arrow_press("down_arrow_press")
         
-plotme(theta, psi, phi, arr)
+    def on_L3_left(self, value):
+        leftjoyleft("L3_left", value)
+        
+    def on_L3_right(self, value):
+        leftjoyright("L3_right", value)
 
-time.sleep(5)
+    def on_L3_up(self, value):
+        leftjoyup("L3_up", value)
+        
+    def on_L3_down(self, value):
+        leftjoydown("L3_down", value)
 
-'''    
-for i in range(10, 0, -1):
-    theta = i*d2r  #pitch
-    psi   = 0*d2r   #yaw
-    phi   = 0*d2r  #roll
-    plotme(theta, psi, phi, arr)
-    #time.sleep(0.1)
-    print("i ",i)
-'''    
-'''
-theta = 10*d2r  #pitch
-psi   = 10*d2r   #yaw
-phi   = 10*d2r  #roll
-plotme(theta, psi, phi, arr)
-time.sleep(0.5)
-theta = 10*d2r  #pitch
-psi   = -10*d2r   #yaw
-phi   = 10*d2r  #roll
-plotme(theta, psi, phi, arr)
-time.sleep(0.5)
-theta = 10*d2r  #pitch
-psi   = 0*d2r   #yaw
-phi   = 10*d2r  #roll
-plotme(theta, psi, phi, arr)
-time.sleep(5)
-'''
+    def on_R3_left(self, value):
+        rightjoyleft("R3_left", value)
+        
+    def on_R3_right(self, value):
+        rightjoyright("RL3_right", value)
+    '''
+    def on_x_press(self):
+        print("on_x_Press")
+        test()
+        
+    def on_triangle_press(self):
+        print("on_triangle_press")
+        initialize()
+    '''
+
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+controller.listen(timeout=60)
